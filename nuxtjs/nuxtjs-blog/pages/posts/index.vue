1331f8eced24aa8c3fa7aafadca63548332c4d4a
Lee Smith <jeremyleesmith@gmail.com>
2018-10-05 09:15:41 -0500

    Use asyncData for posts page

--- nuxtjs/nuxtjs-blog/pages/posts/index.vue
+++ nuxtjs/nuxtjs-blog/pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -10,6 +10,28 @@ import PostList from "@/components/Posts/PostList";
 export default {
   components: {
     PostList
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is my first post!",
+            thumbnail:
+              "https://static.techspot.com/images2/news/bigimage/2016/11/2016-11-21-image-2.jpg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is my second post!",
+            thumbnail:
+              "https://static.techspot.com/images2/news/bigimage/2016/11/2016-11-21-image-2.jpg"
+          }
+        ]
+      });
+    }, 1500);
   }
 };
 </script>
