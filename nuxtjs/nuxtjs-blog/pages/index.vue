a163bf9e2a110644d8f9bae70708700972c8fc5f
Lee Smith <jeremyleesmith@gmail.com>
2018-10-05 09:05:30 -0500

    Load posts using asyncData method

--- nuxtjs/nuxtjs-blog/pages/index.vue
+++ nuxtjs/nuxtjs-blog/pages/index.vue
@@ -14,26 +14,34 @@ export default {
   components: {
     PostList
   },
-  data() {
-    return {
-      loadedPosts: [
-        {
-          id: "1",
-          title: "First Post",
-          previewText: "This is my first post!",
-          thumbnail:
-            "https://static.techspot.com/images2/news/bigimage/2016/11/2016-11-21-image-2.jpg"
-        },
-        {
-          id: "2",
-          title: "Second Post",
-          previewText: "This is my second post!",
-          thumbnail:
-            "https://static.techspot.com/images2/news/bigimage/2016/11/2016-11-21-image-2.jpg"
-        }
-      ]
-    };
-  }
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is my first post!",
+            thumbnail:
+              "https://static.techspot.com/images2/news/bigimage/2016/11/2016-11-21-image-2.jpg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is my second post!",
+            thumbnail:
+              "https://static.techspot.com/images2/news/bigimage/2016/11/2016-11-21-image-2.jpg"
+          }
+        ]
+      });
+    }, 1500);
+  },
+  // data() {
+  //  return {
+  //    loadedPosts: []
+  //  };
+  // },
+  created() {}
 };
 </script>
 
