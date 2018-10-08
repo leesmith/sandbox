7a77a6248377a36226c7ac916093bca6c79c4164
Lee Smith <jeremyleesmith@gmail.com>
2018-10-08 11:19:57 -0500

    Add asyncData to single post

--- nuxtjs/nuxtjs-blog/pages/posts/_id/index.vue
+++ nuxtjs/nuxtjs-blog/pages/posts/_id/index.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="single-post-page">
     <section class="post">
-      <h1 class="post-title">Title</h1>
+      <h1 class="post-title">{{ loadedPost.title }}</h1>
       <div class="post-details">
-        <div class="post-detail">Last updated on XXX</div>
-        <div class="post-detail">Written by NAME</div>
+        <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
+        <div class="post-detail">{{ loadedPost.author }}</div>
       </div>
-      <p class="post-content">Content of the post</p>
+      <p class="post-content">{{ loadedPost.content }}</p>
     </section>
     <section class="post-feedback">
       <p>Shoot me some comments to <a href="mailto:me@example.com">me@example.com</a></p>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: "1",
+          title: "First Post (ID: " + context.route.params.id + ")",
+          previewText: "This is my first post!",
+          author: "Lee Smith",
+          updatedDate: new Date(),
+          content: "Lots of text here for the actual content.",
+          thumbnail:
+            "https://static.techspot.com/images2/news/bigimage/2016/11/2016-11-21-image-2.jpg"
+        }
+      });
+    }, 1000);
+  }
+};
+</script>
 
 <style scoped>
 .single-post-page {
